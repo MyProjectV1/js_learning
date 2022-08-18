bbec3e189599d95bdb850d831c7152afee3fa68d
Default <example@gmail.com>
2022-08-18 21:14:26 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+/* alert('Hello'); */
+
+/* const result = confirm('Are you here?'); */
+
+/* const answer = prompt('Вам есть 18?', '');
+console.log(typeof(answer)); */
+
+/* const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваше фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', ''); */
+
+/* const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+ 
+alert(`Привет, ${user}`); */
+
+/* let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+
+console.log(2*4 == 4+4);
+console.log(2*4 == '8');
+console.log(2*4 === '8');
+console.log(2*4 === 8); */
+
+/* && = и */
+/* || = или */
+
+/* const isChecked = true,
+        isClose = false;
+
+console.log(isChecked && isClose); */
+
+/* const isChecked = true,
+        isClose = false;
+
+console.log(isChecked || isClose); */
+
+/* != - не равен */
